80aef53a335daf342d851a5de87c658d618dfa28
wujunze <itwujunze@163.com>
2019-03-19 17:47:00 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, Panda!"
+}
+
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
