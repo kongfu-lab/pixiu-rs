786b905e0c55185d69db314f576fe3d7e6a92bd2
wujunze <itwujunze@163.com>
2019-03-20 09:20:57 +0800

    SnowFlake ID SERVER

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@
 #[macro_use]
 extern crate rocket;
 
+use flaken::Flaken;
+
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, Panda!"
+fn id() -> String  {
+    let mut flake = Flaken::default().node(10).epoch(0).bitwidths(48, 12);
+    return flake.next().to_string();
 }
 
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![id]).launch();
 }
